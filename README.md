91235019d950edbc0fc92b7913e66405364f02fe
CoffeeCatRailway <dragonlee1002@gmail.com>
2022-05-19 17:58:38 +1000

    :tada: Initial setup

---
+++ README.md
@@ -0,0 +1,2 @@
+This is an empty README-file for the `@rrox-plugins/coffee-train` plugin.
+Please fill this with useful information and a changelog when you update the version.
---
+++ src/controller/index.ts
@@ -0,0 +1,11 @@
+import {Actions, Controller, IPluginController} from '@rrox/api';
+
+export default class CoffeeTrainPlugin extends Controller {
+
+    public async load(controller: IPluginController) {
+    }
+
+    public async unload(controller: IPluginController) {
+
+    }
+}
